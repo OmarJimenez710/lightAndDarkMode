a4c020c815c36c1d1bee31941cb6219b0dbc6260
omarJimenezFlores <omar.jimenez@hixsa.com>
2023-11-09 17:34:30 -0600

    Implementación de dinamismo tanto en el renderizado de información y asi como en el cambio del color en background

--- src/app/dashboard/pages/footer/footer.component.ts
+++ src/app/dashboard/pages/footer/footer.component.ts
@@ -1,53 +1,33 @@
 import { Component } from '@angular/core';
 
 export interface Platillos {
-  nombre : string;
-  clase : string;
+  nombre: string;
+  color: string;
+}
+
+export interface Promociones {
+  prom: string;
 }
 
 const PLATILLOS : Platillos[] = [
-  {
-    nombre: 'Bebidas',
-    clase: 'bebida'
-  },
-  {
-    nombre: 'Desayunos',
-    clase: 'desyunos'
-  },
-  {
-    nombre: 'Carnes y aves',
-    clase: 'carnes'
-  },
-  {
-    nombre: 'Hamburgesas y Sadwiches',
-    clase: 'hamburgesas'
-  },
-  {
-    nombre: 'Pastas',
-    clase: 'pastas'
-  },
-  {
-    nombre: 'Ensaladas',
-    clase: 'enchi'
-  },
-  {
-    nombre: 'Postres',
-    clase: 'postres'
-  },
-  {
-    nombre: 'Entradas',
-    clase: 'entra'
-  },
-  {
-    nombre: 'Pescados',
-    clase: 'pes'
-  },
-  {
-    nombre: 'Comida Mexicana',
-    clase: 'mexican'
-  }
+  { nombre: 'Bebidas', color: '#53AFB5'},
+  { nombre: 'Desayunos', color: '#EF3946'},
+  { nombre: 'Carnes y aves', color: '#F78F36' },
+  { nombre: 'Hamburgesas y Sadwiches', color: '#7E63AB' },
+  { nombre: 'Pastas', color: '#53AFB5' },
+  { nombre: 'Ensaladas', color: '#EF3946' },
+  { nombre: 'Postres', color: '#F78F36' },
+  { nombre: 'Entradas', color: '#7E63AB' },
+  { nombre: 'Pescados', color: '#53AFB5' },
+  { nombre: 'Comida Mexicana', color: '#EF3946' }
 ]
 
+const PROMOCION : Promociones[] = [
+  {prom: 'Come Todo por $186'},
+  {prom: 'Helados 2x1'},
+  {prom: 'Miercoles de bebidas'},
+  {prom: 'Si comes 15 tacos en menos de 15 min todo gratis'}
+]
 
 @Component({
   selector: 'app-footer',
@@ -56,5 +36,6 @@ const PLATILLOS : Platillos[] = [
 })
 export class FooterComponent {
   PLATILLOS = PLATILLOS;
+  PROMOCION = PROMOCION;
   enable = true;
 }
